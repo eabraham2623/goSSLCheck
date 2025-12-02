d17c4d57eff428ee4ef1c706afe0a8f39ca30a63
eabraham2623 <eabraham2623@gmail.com>
2025-12-01 21:52:59 -0500

    Updating main

--- hello_go/main.go
+++ hello_go/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// var <var name> <var_type> = <value>
+	var line string = "simple"
+	fmt.Println("hello world, I am the bests")
+	containsSimple := strings.Contains(line, "simple")
+
+	fmt.Println("\nDoes the string contain 'simple'? %t\n", containsSimple)
 }
